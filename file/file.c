690d6dde1e7a7024a9a30425c9229cd4506a797b
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2020-07-19 09:04:24 -0500

    added filename parse logic used to search for which file contains the row/col being searched for

--- file/file.c
+++ file/file.c
@@ -7,6 +7,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <dirent.h>
+#include <errno.h>
 
 #include "file.h"
 #include "../globals.h"
@@ -63,6 +65,7 @@ struct kt_file *kt_mmap(const char *fname) {
         goto close_fd;
     }
 
+    /* TODO: allocate huge pages */
     dat = mmap(
         NULL,
         map_size,
@@ -94,6 +97,110 @@ fail:
     return NULL;
 }
 
+enum State {
+    TABLE_NAME,
+    COL_NAME,
+    ROW_BEGIN,
+    ROW_END
+};
+
+static char kt_parse_data_filename(
+    const char *str,
+    char **tablename,
+    char **colname,
+    int *rowbegin,
+    int *rowend
+) {
+    int i;
+    int j;
+    int idx;
+    enum State state = TABLE_NAME;
+    *tablename = NULL;
+    *colname = NULL;
+    for (i = 0; str[i] != '\0'; i++) {
+        if ((str[i] == '-') || (str[i] == '.')) {
+            switch (state) {
+                case TABLE_NAME:
+                    *tablename = strdup(str);
+                    (*tablename)[i] = '\0';
+                    state++;
+                    idx = ++i;
+                    break;
+
+                case COL_NAME:
+                    i++;
+                    *colname = strdup(str + idx);
+                    for (j = 0; (*colname)[j] != '-'; j++);
+                    (*colname)[j] = '\0';
+                    state++;
+                    idx = i;
+                    break;
+
+                case ROW_BEGIN:
+                    i++;
+                    *rowbegin = strtol(str + idx, NULL, 10);
+                    if (errno == ERANGE) {
+                        goto fail;
+                    }
+                    state++;
+                    idx = i;
+                    break;
+
+                case ROW_END:
+                    *rowend = strtol(str + idx, NULL, 10);
+                    if (errno == ERANGE) {
+                        goto fail;
+                    }
+                    return 1;
+            }
+        }
+    }
+fail:
+    free(*tablename);
+    free(*colname);
+    return 0;
+}
+
+char *kt_find_file(const char *col, int row) {
+    DIR *dir;
+    struct dirent *ent;
+    char *fname;
+    int rowbegin;
+    int rowend;
+    char *tablename = NULL;
+    char *colname = NULL;
+
+    UNUSED(col);
+    UNUSED(row);
+
+    if ((dir = opendir(storage_dir)) != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            printf("%s\n", ent->d_name);
+            if (!kt_parse_data_filename(
+                ent->d_name,
+                &tablename,
+                &colname,
+                &rowbegin,
+                &rowend)
+            ) {
+                continue;
+            } else {
+                printf("%s %s %d %d\n", tablename, colname, rowbegin, rowend);
+                /* TODO: find the file that contains the correct row and col */
+                free(tablename);
+                free(colname);
+            }
+        }
+        fname = calloc(1, sizeof(char));
+    } else {
+        perror("failed to open storage directory");
+        fname = NULL;
+    }
+
+    closedir(dir);
+    return fname;
+}
+
 #ifdef KT_TEST_MMAP
 int main(void) {
     int i;
@@ -101,6 +208,7 @@ int main(void) {
     const char *str = "this is some data";
     size_t len = strlen(str);
     int status = 0;
+    char *fname = NULL;
 
     file = kt_mmap("tmp.dat");
     if (!file) {
@@ -119,6 +227,8 @@ int main(void) {
         fprintf(stderr, "%s\n", msg);
         status = -1;
     }
+    fname = kt_find_file("A", 30);
+    free(fname);
     return status;
 }
 #endif
--- globals.h
+++ globals.h
@@ -1,6 +1,10 @@
 #ifndef KT_GLOBALS_H
 #define KT_GLOBALS_H
 
+#define UNUSED(x) (void)(x)
+
+const char *storage_dir = "worksheetsdb";
+
 enum kt_constants {
     KT_MAP_SIZE = (2 << 20)
 };
