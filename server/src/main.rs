984d374f4fcb9b31b16f58f07bd9fdc1dbe191f3
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2020-07-26 15:21:13 -0500

    can get individual cell values

--- server/src/main.rs
+++ server/src/main.rs
@@ -10,6 +10,13 @@ fn hello(name: String, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
+#[get("/get/<table>/<col>/<row>")]
+fn get_cell(table: String, col: u64, row: u64) -> Result<String, String> {
+    let file = vecstorage::find_file(table, col, row)?;
+    let x = vecstorage::file_get_int(file, row);
+    Ok(format!("{}", x))
+}
+
 fn lookup_file() -> Result<i32, String> {
     let file = vecstorage::find_file("employee".to_string(), 0, 30)?;
     vecstorage::file_set_int(file, 100, 108743242);
@@ -24,5 +31,5 @@ fn main() {
     let x = vecstorage::print_hello(144);
     println!("i got back: {}", x);
     lookup_file();
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite().mount("/", routes![hello, get_cell]).launch();
 }
