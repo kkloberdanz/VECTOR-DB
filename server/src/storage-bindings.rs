eb5b5ede28a220bed8bd65f0863f24ac86f4804b
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2020-08-02 16:45:17 +0000

    fixed ARM build

--- server/src/storage-bindings.rs
+++ server/src/storage-bindings.rs
@@ -1,12 +1,13 @@
 extern crate libc;
 
 use std::os::raw::c_int;
+use std::os::raw::c_char;
 
 #[link(name = "vectrix-storage")]
 extern "C" {
     pub fn kt_print_hello(i: c_int) -> c_int;
     pub fn kt_file_free(file: *mut libc::c_void) -> c_int;
-    pub fn kt_find_file(table: *const i8, col: u64, row: u64) -> *mut libc::c_void;
+    pub fn kt_find_file(table: *const c_char, col: u64, row: u64) -> *mut libc::c_void;
     pub fn kt_file_set_int(file: *mut libc::c_void, row: u64, value: i64);
     pub fn kt_file_set_float(file: *mut libc::c_void, row: u64, value: f64);
     pub fn kt_file_get_int(file: *mut libc::c_void, row: u64) -> i64;
