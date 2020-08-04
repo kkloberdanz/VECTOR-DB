fb92a884efe5d0d675d0119ece8ef0ea171b75f9
kkloberdanz <kyle.g.kloberdanz@gmail.com>
2020-08-03 19:12:27 -0500

    use double amount of CPUs

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import requests
 from multiprocessing.pool import Pool as P
 import multiprocessing
 
-host = 'http://localhost:8000' 
+host = 'http://localhost:8000'
 path = '/set/int/asdf/0/{row}/{value}'
 
 MAX = 10000
@@ -15,9 +15,9 @@ def insert_num(row):
     if resp.status_code != 200:
         raise Exception('bad status')
 
-with P(multiprocessing.cpu_count()) as workers:
+with P(2 * multiprocessing.cpu_count()) as workers:
     list(workers.map(insert_num, range(0, MAX)))
-        
+
 requests.post(host + f'/sum/asdf/0/0/{MAX}/{MAX + 1}');
 my_sum = int(requests.get(host + f'/get/int/asdf/0/{MAX + 1}').content)
 print('sum is:', my_sum)
