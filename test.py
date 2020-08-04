db9b23a969eb7db424aa7419741708eae260d3d6
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2020-08-03 19:23:55 -0500

    testing as floating point

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ import requests
 from multiprocessing.pool import Pool as P
 import multiprocessing
 
-host = 'http://localhost:8000'
-path = '/set/int/asdf/0/{row}/{value}'
+host = 'http://homeflix.local:8000'
+path = '/set/float/asdf/0/{row}/{value}'
 
-MAX = 10000
+MAX = 1000
 
 def insert_num(row):
     route = host + path.format(row=row, value=row)
@@ -19,7 +19,7 @@ with P(2 * multiprocessing.cpu_count()) as workers:
     list(workers.map(insert_num, range(0, MAX)))
 
 requests.post(host + f'/sum/asdf/0/0/{MAX}/{MAX + 1}');
-my_sum = int(requests.get(host + f'/get/int/asdf/0/{MAX + 1}').content)
+my_sum = float(requests.get(host + f'/get/float/asdf/0/{MAX + 1}').content)
 print('sum is:', my_sum)
-assert(my_sum == sum(range(MAX)))
+assert(my_sum == sum(float(x) for x in range(MAX)))
 print('Ok')
